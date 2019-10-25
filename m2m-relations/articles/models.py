a3c82a02d301b0e01c92f1b71465cf080ac2b571
Rebrina <feurioster@gmail.com>
2019-10-25 22:46:41 +0300

    1v

---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+class Scope(models.Model):
+    article = models.ManyToManyField(Article, through='Membership')
+    tematik = models.CharField(max_length=256, verbose_name='Название')
+    def __str__(self):
+        return self.tematik
+
+class Membership(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='основной', default=False)
+
+    def __str__(self):
+        return '{0}_{1}'.format(self.article, self.scope)
---
+++ m2m-relations/articles/views.py
@@ -0,0 +1,21 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+from .models import Article, Membership, Scope 
+
+def articles_list(request):
+    template = 'articles/news.html'
+    context = {}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    articles = Article.objects.all()
+    context['tags'] = {}
+    for article in articles:
+        context['tags'][article.id] = {}
+        tags = Scope.objects.filter(article=article.id).order_by('-membership__is_main', 'tematik').all()
+        context['tags'][article.id] = tags
+    context['object_list'] = Article.objects.all()
+
+    ordering = '-published_at'
+
+    return render(request, template, context)
